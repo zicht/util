f68b39660b8859641427dbc9bcddf6032771fedb
Gerard van Helden <gerard@zicht.nl>
2013-03-12 19:15:01 +0000

    fixed CS
    git-svn-id: svn://zon/projecten/zicht-libraries/ZichtUtil/trunk@17530 0f35748b-8228-47f0-bbea-889afbbd9a42

--- src/Zicht/Util/Html.php
+++ src/Zicht/Util/Html.php
@@ -20,21 +20,22 @@ class Html
     public static function repair($html)
     {
         $ret = '';
-        for($i = 0; $i < strlen($html);) {
-            if($html[$i] == '<') {
-                if(preg_match('/^<\/?\w+[^>]*?>/', substr($html, $i), $m)) {
+        for ($i = 0; $i < strlen($html);) {
+            if ($html[$i] == '<') {
+                if (preg_match('/^<\/?\w+[^>]*?>/', substr($html, $i), $m)) {
                     $ret .= self::sanitizeTag($m[0]);
                     $i += strlen($m[0]);
-                } else { // assume unclosed tag, escape content.
+                } else {
+                    // assume unclosed tag, escape content.
                     $ret .= '&lt;';
-                    $i ++;
+                    $i++;
                 }
             } else {
                 $ret .= $html[$i];
-                $i ++;
+                $i++;
             }
         }
-        foreach(array_reverse(self::sanitizeTag(null, true)) as $unclosed) {
+        foreach (array_reverse(self::sanitizeTag(null, true)) as $unclosed) {
             $ret .= '</' . $unclosed . '>';
         }
         return $ret;
@@ -51,29 +52,29 @@ class Html
     private static function sanitizeTag($tag, $resetStack = false)
     {
         static $stack = array();
-        if($resetStack) {
+        if ($resetStack) {
             $ret = $stack;
             $stack = array();
             return $ret;
         }
 
         $tag = substr($tag, 1, -1); // strip off leading '<' and trailing '>'
-        if($tag[0] == '/') {
+        if ($tag[0] == '/') {
             preg_match('/^\s*\w+/', substr($tag, 1), $m);
             $closingTag = strtolower($m[0]);
             $localStack = array();
 
             do {
-                if(!count($stack)) {
+                if (!count($stack)) {
                     $stack = array_reverse($localStack);
                     // tag was never opened. Drop closing tag
                     return '';
                 }
                 $top = array_pop($stack);
-                $localStack[]= $top;
-            } while(strtolower($top) != $closingTag);
+                $localStack[] = $top;
+            } while (strtolower($top) != $closingTag);
             $ret = '';
-            foreach($localStack as $needClose) {
+            foreach ($localStack as $needClose) {
                 $ret .= '</' . $needClose . '>';
             }
 
@@ -86,61 +87,59 @@ class Html
 
             $paragraphLevelElements = array('p', 'h6', 'h5', 'h4', 'h3', 'h2', 'h1');
             $inlineElements = array('span', 'b', 'i', 'strong', 'em', 'a');
-            if(count($stack) && in_array($tagName, $paragraphLevelElements)) {
-                while(in_array(end($stack), array_merge($paragraphLevelElements, $inlineElements))) {
+            if (count($stack) && in_array($tagName, $paragraphLevelElements)) {
+                while (in_array(end($stack), array_merge($paragraphLevelElements, $inlineElements))) {
                     $ret .= '</' . array_pop($stack) . '>';
                 }
             }
 
-            if(substr($tag, -1) == '/') {
+            if (substr($tag, -1) == '/') {
                 $isEmpty = true;
             } else {
                 $isEmpty = in_array($tagName, array('br', 'img', 'input'));
             }
-            for($i = strlen($m[0]); $i < strlen($tag);) {
+            for ($i = strlen($m[0]); $i < strlen($tag);) {
                 $preI = $i;
-                while(ctype_space($tag[$i]))
-                    $i ++;
+                while (ctype_space($tag[$i])) {
+                    $i++;
+                }
 
                 $chunk = substr($tag, $i);
 
-                if(preg_match('/^([\w-]+)\s*=\s*\"([^"]*)\"?/', $chunk, $m)) {
-                    $attributes[$m[1]]= $m[2];
+                if (preg_match('/^([\w-]+)\s*=\s*\"([^"]*)\"?/', $chunk, $m)) {
+                    $attributes[$m[1]] = $m[2];
                     $i += strlen($m[0]);
-                } elseif(preg_match('/^([\w-]+)\s*=\s*\'([^\']*)\'?/', $chunk, $m)) {
-                    $attributes[$m[1]]= $m[2];
+                } elseif (preg_match('/^([\w-]+)\s*=\s*\'([^\']*)\'?/', $chunk, $m)) {
+                    $attributes[$m[1]] = $m[2];
                     $i += strlen($m[0]);
-                } elseif(preg_match('/^([\w-]+)=(\S+)/', $chunk, $m)) {
-                    $attributes[$m[1]]= $m[2];
+                } elseif (preg_match('/^([\w-]+)=(\S+)/', $chunk, $m)) {
+                    $attributes[$m[1]] = $m[2];
                     $i += strlen($m[0]);
-                } elseif(preg_match('/^(\w+)/', $chunk, $m)) {
+                } elseif (preg_match('/^(\w+)/', $chunk, $m)) {
                     $attributes[$m[1]] = $m[1];
                     $i += strlen($m[0]);
                 } else {
                     // drop character
-                    $i ++;
+                    $i++;
                 }
                 assert('$i != $preI');
             }
 
             $ret .= '<';
             $ret .= $tagName;
-            if(count($attributes)) {
-                foreach($attributes as $name => $value) {
+            if (count($attributes)) {
+                foreach ($attributes as $name => $value) {
                     $ret .= ' ';
                     $ret .= sprintf('%s="%s"', $name, $value);
                 }
             }
-            if($isEmpty) {
+            if ($isEmpty) {
                 $ret .= ' /';
             } else {
-                $stack[]= $tagName;
+                $stack[] = $tagName;
             }
             $ret .= '>';
             return $ret;
         }
     }
 }
-
-
-
