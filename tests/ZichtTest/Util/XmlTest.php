516b997c00da19759953f7b4d50d3f238a6e6d31
Gerard van Helden <gerard@zicht.nl>
2014-12-11 10:10:45 +0000

    addded test
    git-svn-id: svn://zon/projecten/zicht-libraries/ZichtUtil/trunk@51903 0f35748b-8228-47f0-bbea-889afbbd9a42

---
+++ tests/ZichtTest/Util/XmlTest.php
@@ -0,0 +1,39 @@
+<?php
+/**
+ * @author Gerard van Helden <gerard@zicht.nl>
+ * @copyright Zicht online <http://zicht.nl>
+ */
+namespace ZichtTest\Util;
+
+use Zicht\Util\Xml;
+
+class XmlTest extends \PHPUnit_Framework_TestCase
+{
+    /**
+     * @dataProvider formattedData
+     * @param string $in
+     * @param string $expect
+     * @return void
+     */
+    public function testFormat($in, $expect)
+    {
+        $this->assertEquals($expect, Xml::format($in));
+    }
+
+
+    /**
+     */
+    public function formattedData()
+    {
+        return array(
+            array(
+                '<xml></xml>',
+                "<?xml version=\"1.0\"?>\n<xml/>\n"
+            ),
+            array(
+                '<xml />',
+                "<?xml version=\"1.0\"?>\n<xml/>\n"
+            ),
+        );
+    }
+}
