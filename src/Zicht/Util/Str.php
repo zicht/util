cc575b6352efaad7e9ed5077f28018706f8c43e2
Gerard van Helden <gerard@zicht.nl>
2016-07-13 18:17:52 +0200

    deprecated slugify, which serves more as documentation than anything else

--- src/Zicht/Util/Str.php
+++ src/Zicht/Util/Str.php
@@ -263,28 +263,12 @@ class Str
     /**
      * Slugify a text.
      *
+     * @deprecated Please use Str::systemize()
      * @param string $text
      * @return string
      */
     public static function slugify($text)
     {
-        // replace non letter or digits by -
-        $text = preg_replace('~[^\\pL\d]+~u', '-', $text);
-
-        // trim
-        $text = trim($text, '-');
-
-        // transliterate
-        if (function_exists('iconv')) {
-            $text = iconv('UTF-8', 'ASCII//TRANSLIT', $text);
-        }
-
-        // lowercase
-        $text = strtolower($text);
-
-        // remove unwanted characters
-        $text = preg_replace('~[^-\w]+~', '', $text);
-
-        return $text;
+        return self::systemize($text);
     }
 }
